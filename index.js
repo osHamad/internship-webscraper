8b7f0e4d4b4c2f427ae9010336b6c35418763186
Osama Hamad <osamahama2005@gmail.com>
2025-04-22 10:38:00 -0400

    add handling iframe content - works on iCIMS job boards

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ import { scraper } from "./scraper.mjs"
 // dayforce, workday, n-able, 
 // scraper('https://careers.n-able.com/jobs?stretchUnit=MILES&stretch=10&location=Ottawa,%20ON,%20Canada&woe=7&regionCode=CA', '.job-title-link')
 // scraper('https://fa-evmr-saasfaprod1.fa.ocs.oraclecloud.com/hcmUI/CandidateExperience/en/sites/CX_1/jobs?lastSelectedFacet=TITLES&location=Ottawa%2C+Ontario%2C+Canada&locationId=100000018991137&locationLevel=city&mode=location&radius=25&radiusUnit=MI&selectedTitlesFacet=TRA', '.search-results a', '.job-tile__title')
-scraper('https://careers-kinaxis.icims.com/jobs/search?ss=1&searchRelation=keyword_all&searchLocation=12955-12964-Ottawa&mobile=false&width=1195&height=500&bga=true&needsRedirect=false&jan1offset=-300&jun1offset=-240&in_iframe=1', '.iCIMS_Anchor')
+// scraper('https://careers-kinaxis.icims.com/jobs/search?in_iframe=1&hashed=-625890713&in_iframe=1', '.iCIMS_Anchor', null, '#icims_content_iframe')
--- scraper.mjs
+++ scraper.mjs
@@ -6,11 +6,11 @@ import puppeteer from 'puppeteer';
 // targetPath: the web page where the job listings are
 // linkClass: the name of the css class that contains the url to the application
 // titleClass: the name of the css class that contains the title of the position. null by default. if null, then use linkClass for both
-export async function scraper(targetPath, linkClass, titleClass=null) {
+export async function scraper(targetPath, linkClass, titleClass=null, iframeClass=null) {
   
   // Launch browser and open a browser page (chrome)
-  const browser = await puppeteer.launch({headless:false});  // Use param: { headless: false } to see the actual page rendering
-  const page = await browser.newPage();
+  const browser = await puppeteer.launch();  // Use param: { headless: false } to see the actual page rendering
+  let page = await browser.newPage();
 
   // Go to a specific job site on workday
   // Example: 'https://earlyaccess.dayforcehcm.com/CandidatePortal/en-US/rossvideo'
@@ -20,6 +20,13 @@ export async function scraper(targetPath, linkClass, titleClass=null) {
 
   // Set page size of the browser (wont be visible, but important for consistency)
   await page.setViewport({ width: 1080, height: 1024 });
+
+  // If content is stored in an iframe, redirect the page to be the iframe
+  if (iframeClass) {
+    await page.waitForSelector(iframeClass)
+    const elementHandle = await page.$(`iframe${iframeClass}`);
+    page = await elementHandle.contentFrame();
+  }
 
   // wait for the selector jobclass to be visible
   await page.waitForSelector(linkClass);
