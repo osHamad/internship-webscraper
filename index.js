d1a0112d24ed456c1cdd3ec3e905a45d41cbdc04
Osama Hamad <osamahama2005@gmail.com>
2025-04-23 12:29:22 -0400

    added a control for getting companies listed in a json file

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-import { scraper } from "./scraper.mjs"
+import { scraper } from "./scraper.mjs";
+import { readFile, writeFile } from 'fs/promises';
 
-// works for:
-// dayforce, workday, n-able, 
-// scraper('https://careers.n-able.com/jobs?stretchUnit=MILES&stretch=10&location=Ottawa,%20ON,%20Canada&woe=7&regionCode=CA', '.job-title-link')
-// scraper('https://fa-evmr-saasfaprod1.fa.ocs.oraclecloud.com/hcmUI/CandidateExperience/en/sites/CX_1/jobs?lastSelectedFacet=TITLES&location=Ottawa%2C+Ontario%2C+Canada&locationId=100000018991137&locationLevel=city&mode=location&radius=25&radiusUnit=MI&selectedTitlesFacet=TRA', '.search-results a', '.job-tile__title')
-// scraper('https://careers-kinaxis.icims.com/jobs/search?in_iframe=1&hashed=-625890713&in_iframe=1', '.iCIMS_Anchor', null, '#icims_content_iframe')
+// Read the file
+const rawData = await readFile('companies.json', 'utf-8');
+
+// Parse JSON
+const jsonData = JSON.parse(rawData);
+
+let results = []
+
+
+
+// const a = await scraper("https://fa-evmr-saasfaprod1.fa.ocs.oraclecloud.com/hcmUI/CandidateExperience/en/sites/CX_1/jobs?keyword=intern&lastSelectedFacet=TITLES&location=Ottawa%2C+Ontario%2C+Canada&locationId=100000018991137&locationLevel=city&mode=location&radius=25&radiusUnit=MI&selectedTitlesFacet=TRA", ".search-results > a", ".job-tile__title")
+
+// console.log(a)
+
+for (let company of jsonData) {
+    let jsonResult = { name: company.name, positions: [] }
+    const scraperResults = await scraper(company.jobBoard, company.linkClass, company.titleClass, company.iframeClass)
+    jsonResult.positions.push(scraperResults)
+    results.push(jsonResult)
+}
+
+try {
+    await writeFile('output.json', JSON.stringify(results, null, 2), 'utf-8');
+    console.log('Data written to output.json');
+  } catch (err) {
+    console.error('Error writing file:', err);
+  }
--- scraper.mjs
+++ scraper.mjs
@@ -1,57 +1,65 @@
 import puppeteer from 'puppeteer';
 
-// TODO:  Right now, this only scrapes the first result page
-//        If there are multiple pages, only page 1 is scraped
-
 // targetPath: the web page where the job listings are
 // linkClass: the name of the css class that contains the url to the application
 // titleClass: the name of the css class that contains the title of the position. null by default. if null, then use linkClass for both
+// iframeClass: the name of the iframe's css class/id, if there is one. This is only required if an iframe is used (like in iCIMS) 
 export async function scraper(targetPath, linkClass, titleClass=null, iframeClass=null) {
+  try {
+    
+    // Launch browser and open a browser page (chrome)
+    const browser = await puppeteer.launch();  // Use param: { headless: false } to see the actual page rendering
+    let page = await browser.newPage();
+
+    // Go to a specific job site on workday
+    // Example: 'https://earlyaccess.dayforcehcm.com/CandidatePortal/en-US/rossvideo'
+    await page.goto(targetPath, {
+      waitUntil: 'domcontentloaded',  // wait until DOM loaded
+    });
+
+    // Set page size of the browser (wont be visible, but important for consistency)
+    await page.setViewport({ width: 1080, height: 1024 });
+
+    // If content is stored in an iframe, redirect the page to be the iframe
+    if (iframeClass) {
+      await page.waitForSelector(iframeClass)
+      const elementHandle = await page.$(`iframe${iframeClass}`);
+      page = await elementHandle.contentFrame();
+    }
+
+    // wait for the selector jobclass to be visible
+    await page.waitForSelector(linkClass);
+    const jobLinks = await page.$$(linkClass);
+
+    let jobTitles;
+    if (titleClass) {
+      await page.waitForSelector(titleClass);
+      jobTitles = await page.$$(titleClass); 
+    } else {
+      jobTitles = jobLinks
+    }
+
+
+    // Store results into an array
+    let results = []
+
+    // Loop over all the elements that were returned
+    for (let i = 0; i < jobLinks.length; i++) {
+      // Get the title of each job, as well as the URL to the application page
+      const title = await jobTitles[i].evaluate(el => el.textContent?.trim());
+      const link = await jobLinks[i].evaluate(el => el.href?.trim());
+
+      // Add to result
+      results.push({ title: title, link: link })
+    }
+
+    // close the browser page
+    await browser.close();
+
+    console.log('successfully scraped: ', targetPath)
+    return results
   
-  // Launch browser and open a browser page (chrome)
-  const browser = await puppeteer.launch();  // Use param: { headless: false } to see the actual page rendering
-  let page = await browser.newPage();
-
-  // Go to a specific job site on workday
-  // Example: 'https://earlyaccess.dayforcehcm.com/CandidatePortal/en-US/rossvideo'
-  await page.goto(targetPath, {
-    waitUntil: 'domcontentloaded',  // wait until DOM loaded
-  });
-
-  // Set page size of the browser (wont be visible, but important for consistency)
-  await page.setViewport({ width: 1080, height: 1024 });
-
-  // If content is stored in an iframe, redirect the page to be the iframe
-  if (iframeClass) {
-    await page.waitForSelector(iframeClass)
-    const elementHandle = await page.$(`iframe${iframeClass}`);
-    page = await elementHandle.contentFrame();
+  } catch(e) {
+    console.log(`Error occured while scraping (${targetPath}): ${e}`)
   }
-
-  // wait for the selector jobclass to be visible
-  await page.waitForSelector(linkClass);
-  const jobLinks = await page.$$(linkClass);
-
-  let jobTitles;
-  if (titleClass) {
-    await page.waitForSelector(titleClass);
-    jobTitles = await page.$$(titleClass); 
-  } else {
-    jobTitles = jobLinks
-  }
-
-
-  // Loop over all the elements that were returned
-  for (let i = 0; i < jobLinks.length; i++) {
-    // Get the title of each job, as well as the URL to the application page
-    const title = await jobTitles[i].evaluate(el => el.textContent?.trim());
-    const link = await jobLinks[i].evaluate(el => el.href?.trim());
-
-    // For now, we are only printing to console
-    console.log("This is a title: ", title);
-    console.log("This is a link: ", link)
-  }
-
-  // close the browser page
-  await browser.close();
 }
