aa674b3f56b321aebdcd3f0208650fbbd2fdab50
Osama Hamad <osamahama2005@gmail.com>
2025-04-26 13:23:52 -0400

    added cron scheduler

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import cron from 'node-cron';
 import companyRouter from './routes/companyRouter.mjs'
+import listingRouter from './routes/listingRouter.mjs'
+import * as scraperService from './services/scraperService.mjs';
 
 // Create express server and define env variables
 const app = express();
@@ -10,6 +13,15 @@ app.use(express.json());
 
 // Add routers
 app.use('/company', companyRouter);
+app.use('/listing', listingRouter);
+
+// Schedule webscrape task
+// Runs every day at midnight
+cron.schedule('0 0 * * *', async () => {
+  const allJobs = await scraperService.scrapeAllJobs();
+  const addedJobs = await scraperService.addManyUnique(allJobs);
+  console.log('jobs added: ', addedJobs);
+})
 
 // Start server
 app.listen(PORT, () => {
