a2840ca3d0e71fc1e0097fd7435364eb79f2fb7d
Osama Hamad <osamahama2005@gmail.com>
2025-04-28 17:56:05 -0400

    add front end compatibility

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
+// Import node modules
 import express from 'express';
 import cron from 'node-cron';
+import path from 'path'
+
+// Import express routers
 import companyRouter from './routes/companyRouter.mjs'
 import listingRouter from './routes/listingRouter.mjs'
+import clientRouter from './routes/clientRouter.mjs'
+
+// Import utils/services/misc
 import * as scraperService from './services/scraperService.mjs';
+import { __dirname } from './utils/paths.mjs'
 
 // Create express server and define env variables
 const app = express();
 const PORT = process.env.PORT || 3000
 
-// Use required middleware
+// Configure ejs views
+app.set('view engine', 'ejs')
+
+// Use parsing middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Add a static directory for public files (js, css, etc)
+app.use(express.static('public'));
 
 // Add routers
 app.use('/company', companyRouter);
 app.use('/listing', listingRouter);
+app.use('/', clientRouter);
 
 // Schedule webscrape task
 // Runs every day at midnight
