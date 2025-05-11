5e9e0ce28e409a513b95731e5820803df7189778
Osama Hamad <osamahama2005@gmail.com>
2025-05-11 11:27:06 -0400

    added new script, css, partial view for notifications

---
+++ public/js/scripts.js
@@ -0,0 +1,10 @@
+function displayNotification(status, message) {
+    const notification = document.getElementById('notification');
+    if (notification) {
+        notification.classList.add(status)
+        notification.innerText = message
+        setTimeout(() => notification.classList.add('show'), 200); // fade in
+        setTimeout(() => notification.classList.remove('show'), 3000); // fade out
+        setTimeout(() => notification.innerHTML, 3500); // remove from DOM
+    }
+}
